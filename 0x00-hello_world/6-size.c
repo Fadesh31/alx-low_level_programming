ed021bb0211d4eb57022e4626d21e0a09efe8f15
Fadesh31 <haikay31@gmail.com>
2022-09-08 20:07:03 -0700

    my work

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,18 +3,17 @@
  * main - main block
  * Return: 0
  */
-int main(void)
+int main (void)
 {
 	int i;
-	long int l;
-	long int ll;
-	float d;
 	char c;
+	long int l;
+	long long int ll;
+	float f;
 
-	printf("Size of a char: %lu.\n",(unsigned long)sizeof(c));
-	printf("Size of an int: %lu.\n",(unsigned long)sizeof(i));
-	printf("Size if a long int: %lu.\n",(unsigned long)sizeof(l) );
-	printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(ll));
-	printf("Size of a float: %lu.\n", (unsigned long)sizeof(d));
-	return (0);
+	printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu.\n", (unsigned long)sizeof(l));
+	printf("Sizef a loong long int: %lu.\n", (unsigned long)sizeof(ll));
+	printf ("Size of a folat: %lu.\n", (unsigned long)sizeof(f));
 }
