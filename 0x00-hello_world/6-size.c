cf6ef4bab2d643e7cc3648d04fd2b9b133926ad5
Fadesh31 <haikay31@gmail.com>
2022-09-08 17:48:18 -0700

    My work

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
-/**
- * main - Prints 'Size of different variables with various types'
- * return: ALways 0 (success)
- */
-int main(void)
-{
-	int i;
-	char c;
-	float d;
-	long int l;
-	long long int ll;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu bytes(s)\n", sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-	printf("Size of a float: %lu byte(s)\n", sizeof(d));
-	return (0);
-}	
-	
+/**
+ *
+ *  * main - Prints "Size of different variables with various data types"
+ *
+ *   * Return: Always 0 (success)
+ *
+ *    */
+
+int main(void)
+
+{
+
+		int i;
+
+			long int l;
+
+				long long int ll;
+
+					float d;
+
+						char c;
+
+
+
+							printf("Size of a char: %lu byte(s)\n", sizeof(c));
+
+								printf("Size of an int: %lu byte(s)\n", sizeof(i));
+
+									printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+
+										printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+
+											printf("Size of a float: %lu byte(s)\n", sizeof(d));
+
+												return (0);
