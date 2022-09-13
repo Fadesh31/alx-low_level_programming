0715dcf0142c8c8d0286d84e0f088a00f767a47b
Fadesh31 <haikay31@gmail.com>
2022-09-13 12:40:01 -0700

    C functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,8 @@
  */
 int main(void)
 {
-	char text [9] = "_putchar";
+	_putchar('_putchar');
+	_putchar('\n');
 
-	int i = 0;
-
-	for(i = '0'; i < '8'; i++)
-	{
-		putchar(text);
-	}
-	putchar('\n');
 	return (0);
 }
