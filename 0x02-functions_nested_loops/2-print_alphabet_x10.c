db09ec14c59925848ae69a0ddc053e092776a948
Fadesh31 <haikay31@gmail.com>
2022-09-13 14:42:38 -0700

    C functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,19 @@
  */
 void print_alphabet_x10(void)
 {
-	char c
-	int i = 0
+	char c;
+	int i = 0;
 
-	while (i <9)
+	while (i <= 9)
 	{
 		for (c = 'a'; c <= 'z'; c++)
+		{
 			_putchar(c);
-	}
-	_putchar('\n');
+		}
 
-	i++;
+		_putchar('\n');
+
+		i++;
+	}
+
 }
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,7 +8,7 @@ int _islower(int c)
 {
 int i = 'a';
 
-for (i = 'a'; i <= z; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
 /* refer int c*/
 if (c == i)
