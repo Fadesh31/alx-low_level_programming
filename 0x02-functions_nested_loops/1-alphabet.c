26591984b44bf27ec46bb2a6dfd3c59cd39bfc37
Fadesh31 <haikay31@gmail.com>
2022-09-13 13:13:06 -0700

    C functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - main block
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	char ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0)
+}
