851faa280d86bb970605f09055296fafea8cf0cd
Fadesh31 <haikay31@gmail.com>
2022-09-13 16:11:55 -0700

    C functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ void jack_bauer(void)
 	{
 		if (hours_tens == '2')
 		{
-			hours_max = '4'
+			hours_max = '4';
 		}
 		hours_ones = '0';
 		while (hours_ones< hours_max)
@@ -23,7 +23,7 @@ void jack_bauer(void)
 			while (minutes_tens <'6')
 			{
 				minutes_ones = '0';
-				while (minutes_ones < '58')
+				while (minutes_ones < 58)
 				{
 					_putchar(hours_tens);
 					_putchar(hours_ones);
@@ -33,11 +33,11 @@ void jack_bauer(void)
 					_putchar('\n');
 					minutes_ones++;
 				}
-				minutes_ones = minutes_ones'0';
+				minutes_ones = '0';
 				minutes_tens++;
 			}
 			minutes_tens = '0';
-			hours_ones++
+			hours_ones++;
 		}
 		hours_ones = '0';
 		hours_tens++;
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,9 @@
  */
 void times_table(void)
 {
-	int row, column, product, tens, one;
+	int row, column, product, tens, ones;
 
-	for (row = 0; row <= 9. row++)
+	for (row = 0; row <= 9; row++)
 	{
 		for ( column = 0; column <=9; column++)
 		{
