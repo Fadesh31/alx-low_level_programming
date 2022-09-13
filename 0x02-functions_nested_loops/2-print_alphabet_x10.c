0ad74d5d86193108d8189aa765206f5ba0819ba0
Fadesh31 <haikay31@gmail.com>
2022-09-13 14:33:26 -0700

    C functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print alphets x10
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c
+	int i = 0
+
+	while (i <9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+	}
+	_putchar('\n');
+
+	i++;
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - entry point
+ * Description: prints absolute value of an integer
+ * @c: n is an integer
+ * Return: int
+ */
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else 
+	{
+		return (n);
+	}
+}
