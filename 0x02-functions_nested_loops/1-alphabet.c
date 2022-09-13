0272051f14b068a4e322f9b803ff1beed6f0bd4f
Fadesh31 <haikay31@gmail.com>
2022-09-13 13:27:24 -0700

    C function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,10 +7,9 @@ void print_alphabet(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
 	}
 	_putchar('\n');
-	return;
 }
