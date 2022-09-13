718dee0a55b2ebbaa4b891ba0d55cadcbfc19c67
Fadesh31 <haikay31@gmail.com>
2022-09-13 16:57:22 -0700

    C functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,6 +21,7 @@ void print_times_table(int n)
 					_putchar(32);
 					_putchar((z / 100) + '0');
 					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
 				else if (z > 9)
 				{
