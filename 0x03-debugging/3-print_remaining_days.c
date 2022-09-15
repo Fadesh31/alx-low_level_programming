ad70a766fad33abe536faaa8ddaa87f790050fd6
Fadesh31 <haikay31@gmail.com>
2022-09-15 14:47:05 -0700

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,7 +18,7 @@ void print_remaining_days(int month, int day, int year)
 			day++;
 		}
 
-		printf("Day of te year: %d\n", day);
+		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
