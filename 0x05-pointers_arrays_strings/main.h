9063981303107e645f12587a9abffa97abdb1caf
Fadesh31 <haikay31@gmail.com>
2022-09-19 08:47:42 -0700

    Pointer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,3 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
-
-#include <stdio.h>
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,5 +8,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif
