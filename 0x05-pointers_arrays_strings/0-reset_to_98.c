6904c6abdad411e3782d383abcd402099551a2b4
Fadesh31 <haikay31@gmail.com>
2022-09-19 08:29:29 -0700

    Pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,17 +7,5 @@
  */
 void reset_to_98(int *n)
 {
-	int n;
-	int *p;
-
-	n = 402;
-	p = &n;
-
-	_putchar("Value of 'n': %d\n", n);
-	_putchar("Address of 'n': %p\n", &n);
-	_putchar("Value of 'p': %p\n", p);
-	*p = 98;
-	_putchar("Value of 'n': %d\n", n);
-
-	return (0);
+	*n = 98
 }
