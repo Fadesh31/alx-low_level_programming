5d2c706732081637b29a731c113ecf67c53c9076
Fadesh31 <haikay31@gmail.com>
2022-09-22 03:49:42 -0700

    More pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,15 +9,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int index = 0, dest_len = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	;
+	while (dest[index++])
+		dest_len++;
 
-	for (j = 0; j < n && src[j] != '\0'; j++)
-		dest[i + j] = src[j];
-
-	dest[i] = '\0';
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
