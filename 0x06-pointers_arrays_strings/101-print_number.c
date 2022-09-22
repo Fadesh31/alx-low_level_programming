d889fe3b2b8204049092acc46392e562e96df34f
Fadesh31 <haikay31@gmail.com>
2022-09-22 06:15:02 -0700

    More pointer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,7 +4,7 @@
  * _abs - returns absolute value of given int
  * @n: number for which we want to get the absolute value
  *
- * @n: number for which we want to get the absolute value
+ * Return: absolute value of number n
  */
 int _abs(int n)
 {
