356f9f16ff330193dcb9f965ad788e9fdb19a351
Fadesh31 <haikay31@gmail.com>
2022-09-22 02:07:06 -0700

    More pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,13 +16,14 @@ char *_strcat(char *dest, char *src)
 	{
 		i++;
 	}
+	
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
 	}
-	dest[i] != '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
