994c0c8fbdf86740ddbf710893124cd6ac41eebc
Fadesh31 <haikay31@gmail.com>
2022-09-26 10:51:18 -0700

    Pointer

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,13 +8,13 @@
  * Return: *s
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int count;
+	unsigned int i;
 
-	for (count = 0; count < n; count++)
+	for (i = 0; i < n; i++)
 	{
-		s[count] = b;
+		s[i] = b;
 	}
 
 	return (s);
