3ebf45b0471a0813c014552f08ef1a3ed020472a
Fadesh31 <haikay31@gmail.com>
2022-09-26 10:22:31 -0700

    Pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,19 +4,17 @@
  * *_memset - stores n bytes of memory with a constant value
  * @s: pointer to the memory areaa
  * @n: number of bytes to store
+ * @b: constant value
  * Return: memory area
  */
 
 char *_memset(char *s, char b, unsigned int n);
 {
-	int size = n;
+	unsigned int i;
 
-	if (size > 0)
+	for (i = 0; i < n; i++)
 	{
-	int i;
-
-	for (i = 0; i < size; i++)
-	s[i]= b;
+		s[i] = b;
 	}
 
 	return (s);
