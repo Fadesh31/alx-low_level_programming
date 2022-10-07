a086fcaa9882f12f092fe3186cab9cb4b0773848
Fadesh31 <haikay31@gmail.com>
2022-10-07 14:00:54 -0700

    Malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,14 +3,16 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: number of bytes to be malloced
- * Return: returns a void pointer
+ * Return: returns 98 or a pointer
  */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
 	ptr = malloc(b);
-	if (!ptr)
+	
+	if (ptr == NULL)
 		exit(98);
+
 	return (ptr);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef "FILE_MAIN"
-#define "FILE_MAIN"
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 #include <stdlib.h>
 #include <stddef.h>
@@ -10,9 +10,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);int _strlen(char *);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int onlyNumbers(char *c);
-void multiply(char* s1, char* s2);
+void errors(void);
+int is_digit(char *s);
 
 #endif
