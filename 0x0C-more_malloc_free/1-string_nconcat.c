ebace5f2ddbe4c5c690ef4ab663c14af38feff37
Fadesh31 <haikay31@gmail.com>
2022-10-07 14:16:06 -0700

    Malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,46 +2,50 @@
 
 /**
  * string_nconcat - concatenates two strings
- * @s1: destination string
- * @s2: source string
+ * @s1: first string
+ * @s2: second string
  * @n: number of bytes from s2 to be copied
- * Return: concatenated string
+ * Return: return pointer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, size, len1, len2;
+	unsigned int len1 = 0, len2 = 0, i;
 	char *ptr;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
+	
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+
 	if (n > len2)
 		n = len2;
-	size = len1 + n;
-	ptr = malloc(sizeof(char) * size + 1);
-	if (!ptr)
-		return (NULL);
-	for (i = 0; i < len1; i++)
+	ptr = malloc((len1 + n + 1) * sizeof(char));
+
+	if (ptr == NULL)
+		return (0);
+
+	for (i 0; i < len1; i++)
+	{
 		ptr[i] = s1[i];
-	for (j = 0; j < n; j++, i++)
-		ptr[i] = s2[j];
-	ptr[i] = 0;
-	return (ptr);
-}
+	}
+	
+	for (; i < (len1 + n); i++)
+	{
+		ptr[i]= s2[i - len1];
+	}
 
-/**
- * strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	char *p = s;
+	ptr[i] = '\0'
 
-	while (*s)
-		s++;
-	return (s - p);
+		return (ptr);
 }
