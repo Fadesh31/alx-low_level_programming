9f9024ed680f97e6cfed051951f9f0d08846531b
Fadesh31 <haikay31@gmail.com>
2022-10-07 13:50:27 -0700

    Malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,8 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef "FILE_MAIN"
+#define "FILE_MAIN"
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
